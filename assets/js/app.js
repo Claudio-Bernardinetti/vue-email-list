30606f5f2c2373f9d32284d3e8f27b1e190efd03
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-09-20 14:30:24 +0100

    add for loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,23 @@ createApp({
 
         return {
            
-            message: 'ciao'
+            generatesEmail: [],
 
         }
     },
     mounted(){
-        console.log('ciao');
+        
+        for (let i = 0; i < 10; i++) {
+            
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+               this.generatesEmail.push (response.data.response);
+            })
+            
+        }
+
+
     }
 
 }).mount('#app');
