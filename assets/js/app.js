269a30977dd2b49f66db109772c71dfa4330b020
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-09-20 15:30:12 +0100

    add success

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,21 +7,25 @@ createApp({
 
         return {
            
-            generatesEmail: []
+            generatesEmail: [], // Array per memorizzerà le email generate
+            success: false, // Segnalatore che indicherà quando tutte le email sono state generate
 
         }
     },
     mounted(){
         
-        for (let i = 0; i < 10; i++) {
-            
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => {
-               this.generatesEmail.push (response.data.response);
+        for (let i = 0; i < 10; i++) { // Ciclo for per fare 10 richieste all'API
 
-            })
-            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail') // Richiesta GET all'API
+
+                .then(response => {
+                    this.generatesEmail.push(response.data.response); // Quando la richiesta ha successo, aggiungiamo l'email generata all'array generatesEmail
+
+                    if (this.generatesEmail.length === 10) { 
+                        this.success = true; // Se le 10 email sono generate impostiamo success a true
+                    }
+                });
+
         }
 
     }
