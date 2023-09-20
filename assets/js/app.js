4ab54665a2a177da32e5612ac18012283e10249d
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-09-20 16:45:58 +0100

    add solution without flag

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ createApp({
         return {
            
             generatesEmail: [], // Array per memorizzerà le email generate
-            success: false, // Segnalatore che indicherà quando tutte le email sono state generate
+            // success: false, // Segnalatore che indicherà quando tutte le email sono state generate
 
         }
     },
@@ -21,9 +21,9 @@ createApp({
                 .then(response => {
                     this.generatesEmail.push(response.data.response); // Quando la richiesta ha successo, aggiungiamo l'email generata all'array generatesEmail
 
-                    if (this.generatesEmail.length === 10) { 
-                        this.success = true; // Se le 10 email sono generate impostiamo success a true
-                    }
+                    // if (this.generatesEmail.length === 10) { 
+                    //     this.success = true; // Se le 10 email sono generate impostiamo success a true
+                    // }
                 });
 
         }
